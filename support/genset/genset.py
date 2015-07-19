192cb2ec8da33b6f63bc1317f24a389e239d0dd7
Michael Hope <mlhx@google.com>
2015-07-19 14:38:43 +0200

    genset: ran pyformat.
    Signed-off-by: Michael Hope <mlhx@google.com>

--- support/genset/genset.py
+++ support/genset/genset.py
@@ -1,7 +1,8 @@
-"""Example of a parser that handles a subset of C and can extract
-structs and typedefs for later generation.
+"""Example of a parser that handles a subset of C and can extract structs and
+typedefs for later generation.
 
 See test.h for example input.
+
 """
 
 import fileinput
@@ -16,24 +17,27 @@ from pyparsing import *
 
 BaseType = collections.namedtuple('BaseType', 'name size bits signed')
 
+
 def make_base_types():
     types = {
         'char': BaseType('char', 1, 8, True),
-        }
+    }
     for size in 1, 2, 4:
         for signed in True, False:
-            bits = size*8
+            bits = size * 8
             name = '{}int{}_t'.format('' if signed else 'u', bits)
             types[name] = BaseType(name, size, bits, signed)
     return types
 
 BASE_TYPES = make_base_types()
 
+
 def find_type(name):
     if name in BASE_TYPES:
         return BASE_TYPES[name]
     else:
         return BaseType(name, -1, -1, False)
+
 
 def first(v):
     if len(v) == 0:
@@ -43,10 +47,12 @@ def first(v):
     else:
         return ' '.join(v)
 
+
 def parse_number(tokens):
     return int(tokens[0])
 
 Comments = collections.namedtuple('Comments', 'text fields')
+
 
 def parse_comments(tokens):
     text = []
@@ -62,33 +68,40 @@ def parse_comments(tokens):
 
 Enum = collections.namedtuple('Enum', 'name fields comments')
 
+
 def parse_enum(tokens):
     return Enum(tokens.name, list(tokens.fields), first(tokens.comments))
 
 EnumField = collections.namedtuple('EnumField', 'name value comments')
+
 
 def parse_enum_field(tokens):
     return EnumField(tokens.name, first(tokens.value), first(tokens.comments))
 
 Typedef = collections.namedtuple('Typedef', 'name alias')
 
+
 def parse_typedef(tokens):
     return Typedef(tokens.name, tokens.alias)
 
 Struct = collections.namedtuple('Struct', 'type name fields comments')
+
 
 def parse_struct(tokens):
     return Struct('Struct', tokens.name, list(tokens.fields), first(tokens.comments))
 
 StructField = collections.namedtuple('StructField', 'type name count comments')
 
+
 def parse_struct_field(tokens):
     return StructField(find_type(tokens.type), tokens.name, tokens.count, first(tokens.comments))
 
 CommentField = collections.namedtuple('CommentField', 'name value')
 
+
 def parse_comment_field(tokens):
     return CommentField(tokens.name, tokens.value)
+
 
 def parse_count(tokens):
     return first(tokens)
@@ -118,9 +131,10 @@ struct_field = (
     Optional(comments).setResultsName('comments')
     + identifier.setResultsName('type')
     + identifier.setResultsName('name')
-    + Optional(lbracket + Optional(number) + rbracket).setParseAction(parse_count).setResultsName('count')
+    + Optional(lbracket + Optional(number) +
+               rbracket).setParseAction(parse_count).setResultsName('count')
     + semi
-    )
+)
 struct_field.setParseAction(parse_struct_field)
 
 struct_decl = (
@@ -133,14 +147,14 @@ struct_decl = (
     + Optional(comments)
     + rbrace
     + ZeroOrMore(identifier)
-    )
+)
 struct_decl.setParseAction(parse_struct)
 
 enum_field = (
     Optional(comments).setResultsName('comments')
     + identifier.setResultsName('name')
     + Optional(equals + number).setResultsName('value')
-    )
+)
 enum_field.setParseAction(parse_enum_field)
 
 enum_decl = (
@@ -152,32 +166,35 @@ enum_decl = (
     + Optional(comma)
     + Optional(comments)
     + rbrace
-    )
+)
 enum_decl.setParseAction(parse_enum)
 
 typedef_decl = (
     typedef
     + identifier.setResultsName('name')
     + identifier.setResultsName('alias')
-    )
+)
 typedef_decl.setParseAction(parse_typedef)
 
 schema = ZeroOrMore((struct_decl | typedef_decl | enum_decl) + semi)
 
-comment_field =(
+comment_field = (
     identifier.setResultsName('name')
     + colon
     + (number | identifier).setResultsName('value')
-    )
+)
 comment_field.setParseAction(parse_comment_field)
 
 sub_comments = delimitedList(comment_field, delim=';')
 
+
 def constname(v):
     return re.sub(r'([a-z])([A-Z])', r'\1_\2', v).upper()
 
+
 def ucfirst(v):
     return v[0].upper() + v[1:]
+
 
 def makeargs(v):
     for field in v:
@@ -186,12 +203,14 @@ def makeargs(v):
         else:
             yield '{} {}'.format(field.type.name, field.name)
 
+
 def totalsize(v):
     type = v.type
     if v.count:
         return '{}*{}'.format(v.count, type.size)
     else:
         return type.size
+
 
 def norm(v):
     """Turn a typedef or struct name into the normalised name."""
@@ -204,7 +223,8 @@ def norm(v):
 types = {
     'Enum': Enum,
     'Struct': Struct,
-    }
+}
+
 
 def main():
     strict = False
@@ -214,7 +234,7 @@ def main():
     parser.add_argument('-t', type=argparse.FileType('r'))
     parser.add_argument('src', type=argparse.FileType('r'))
     args = parser.parse_args()
-    
+
     env = jinja2.Environment(trim_blocks=True)
     env.filters.update({
         'constname': constname,
@@ -222,7 +242,7 @@ def main():
         'makeargs': makeargs,
         'totalsize': totalsize,
         'norm': norm,
-        })
+    })
     t = env.from_string(args.t.read())
     decls = []
 
@@ -238,7 +258,7 @@ def main():
     rendered = t.render(decls=decls,
                         types=types,
                         isinstance=isinstance,
-                    )
+                        )
 
     if args.o:
         with open(args.o, 'w') as f:
